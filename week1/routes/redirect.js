485b7e36aa38d1cc760706dc8f7b040c08f7344a
DamonHuang6 <yuxiang_huang@outlook.com>
2016-09-21 14:12:50 -0400

    1st

--- week1/routes/redirect.js
+++ week1/routes/redirect.js
@@ -1,3 +1,14 @@
 /**
  * Created by Damon on 16/9/19.
  */
+var express = require("express");
+var router = express.Router();
+var urlService = require("../services/urlService");
+
+router.get("*", function(req, res) {
+    var shortUrl = req.originalUrl.slice(1);
+    var longUrl = urlService.getLongUrl(shortUrl);
+    res.redirect(longUrl);
+});
+
+module.exports = router;
--- week1/routes/rest.js
+++ week1/routes/rest.js
@@ -1,14 +1,15 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
+var urlService = require('../services/urlService');
 router.post('/urls', jsonParser, function (req, res) {
     var longUrl = req.body.longUrl;
     var shortUrl = urlService.getShortUrl(longUrl);
     res.json({
         shortUrl: shortUrl,
         longUrl: longUrl
-    })
+    });
 });
 
 module.exports = router;
