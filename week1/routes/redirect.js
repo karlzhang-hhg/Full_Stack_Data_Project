826483f9895c907684727a36c70dd80858f0f80f
Yuxiang <DamonHuang6@users.noreply.github.com>
2016-09-20 20:27:32 -0400

    day1

--- week1/routes/redirect.js
+++ week1/routes/redirect.js
@@ -1,14 +1,3 @@
-var express = require('express');
-var router = express.Router();
-var urlService = require('../services/urlService');
-
-router.get("*", function (req, res) {
-    var shortUrl = req.originalUrl.slice(1);
-
-    var longUrl = urlService.getLongUrl(shortUrl);
-    if (longUrl) {
-        res.redirect(longUrl);
-    }
-});
-
-module.exports = router;
+/**
+ * Created by Damon on 16/9/19.
+ */
--- week1/routes/rest.js
+++ week1/routes/rest.js
@@ -1,16 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
-var urlService = require('../services/urlService');
-
-router.post("/urls", jsonParser, function (req, res) {
+router.post('/urls', jsonParser, function (req, res) {
     var longUrl = req.body.longUrl;
     var shortUrl = urlService.getShortUrl(longUrl);
     res.json({
         shortUrl: shortUrl,
         longUrl: longUrl
-    });
+    })
 });
 
 module.exports = router;
